4d75c28eb65916220349b47caf549808491ecaf8
snoor_madih <snoor.madih@gmail.com>
2021-06-14 09:08:40 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -12,39 +12,52 @@ class Timer extends Component {
 
   //Your code here
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
+    this.timer.current.style.width = 240+this.state.time*5/1000+"px"
+    this.timer.current.style.height = 150+this.state.time*5/1000+"px"
+  }
+
   componentDidMount() {
-    this.interval = setInterval(
-      this.clockTick,
-      this.props.updateInterval * 1000
-    );
+    this.interval = setInterval(this.clockTick, this.props.updateInterval*1000)
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   render() {
-    const { time, color, logText } = this.state;
+    const { time, color, className, logText } = this.state
     return (
-      <section className="Timer" style={{ background: color }} ref={this.timer}>
-        <h1>{time}</h1>
-        <button onClick={this.stopClock}>Stop</button>
-        <aside className="logText">{logText}</aside>
-        <small onClick={this.handleClose}>X</small>
+      <section className="Timer" style={{background: color}} ref={this.timer}>
+
+        <h1>{ time }</h1>
+        <button onClick={ this.stopClock }>Stop</button>
+        <aside className="logText">{ logText }</aside>
+        <small onClick={ this.handleClose }>X</small>
+
       </section>
     );
   }
 
+
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + this.props.updateInterval
-    }));
-  };
+    }))
+  }
 
   stopClock = () => {
-    clearInterval(this.interval);
-    this.setState({ className: "hidden" });
-  };
+    clearInterval(this.interval)
+    this.setState({className: "hidden"})
+  }
 
   // for the 'x' button,
   handleClose = () => {
